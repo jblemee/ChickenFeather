a57f3b1248331944d246a9ec484febdd8fa5275c
Jean-Baptiste Lemee <git@lemee.co>
2023-04-09 03:34:53 -1000

    Rewards experience on feather drop

--- src/main/java/co/lemee/chickenfeathermod/ChickenFeatherMod.java
+++ src/main/java/co/lemee/chickenfeathermod/ChickenFeatherMod.java
@@ -1,11 +1,15 @@
 package co.lemee.chickenfeathermod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -17,25 +21,15 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import java.text.MessageFormat;
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(ChickenFeatherMod.MODID)
 public class ChickenFeatherMod {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "chickenfeathermod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-
     private static final String DROPPED_TAG = "chickenfeather:dropped";
 
     public ChickenFeatherMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -46,9 +40,14 @@ public class ChickenFeatherMod {
     @SubscribeEvent
     public void onPlayerEntityInteract(PlayerInteractEvent.EntityInteract entityInteract) {
         if (entityInteract.getTarget() instanceof Chicken chicken) {
-            if (!chicken.getTags().contains(DROPPED_TAG)) {
+            Level level = entityInteract.getLevel();
+            if (!chicken.getTags().contains(DROPPED_TAG) && chicken.shouldDropExperience()) {
                 chicken.addTag(DROPPED_TAG);
                 chicken.spawnAtLocation(new ItemStack(Items.FEATHER));
+                if (level instanceof ServerLevel serverLevel) {
+                    int reward = ForgeEventFactory.getExperienceDrop(chicken, entityInteract.getEntity(), 1);
+                    ExperienceOrb.award(serverLevel, entityInteract.getPos().getCenter(), reward);
+                }
             }
         }
         ;
@@ -66,7 +65,6 @@ public class ChickenFeatherMod {
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
         LOGGER.info("Chicken Feather Mod ACTIVE");
     }
 
@@ -75,7 +73,6 @@ public class ChickenFeatherMod {
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
             LOGGER.info("Chicken Feather Mod SETUP");
         }
     }
